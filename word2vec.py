9743a9af5c1d414663f1be5ac11aeeb36a3b9ae2
xy990 <xy990@nyu.edu>
2017-05-11 09:57:53 -0400

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -5,7 +5,7 @@ from zipfile import ZipFile
 from collections import Counter
 import pickle
 import re
-os.chdir('/Users/muriel820/Downloads/ml-project/cleaned_1880')
+os.chdir('/Users/xy990/Downloads/ml-project/cleaned_1880')
 zipfiles = glob('*zip')
 #all_str =" "
 #year_str =" "
